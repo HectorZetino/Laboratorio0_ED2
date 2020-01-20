8fffa79c489d8089eebce5ed9a029c7d1e9baed7
HectorZetino <h.rzc.zetino@hotmail.es>
2020-01-20 14:03:47 -0600

    Creando Api, Agregando clase Movie, Generando interfaz ICRUD

---
+++ Lab0_ED2/Lab0_ED2/Models/CRUD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab0_ED2.Models
+{
+    public class CRUD
+    {
+        public List<Movie> Movies = new List<Movie>();
+
+        public void Add(Movie New_Movie) {
+            Movies.Add(New_Movie);
+        
+        }
+
+        public List<Movie> ReturnList() {
+            return Movies;
+        }
+
+
+    }
+}
---
+++ Lab0_ED2/Lab0_ED2/Models/ICRUD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab0_ED2.Models
+{
+    interface ICRUD
+    {
+        public void add(Movie movie);
+        public List<Movie> ReturnList();
+    }
+}
