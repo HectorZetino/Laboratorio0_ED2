c616cdaa957acd76e7790f46c44fb7c5c9e8bfef
HectorZetino <h.rzc.zetino@hotmail.es>
2020-01-21 19:43:22 -0600

    Agregando forma de unicamente mostrar las 10 peliculas de la lista, y peliculas sobrantes almacenarla en una lista auxiliar

--- Lab0_ED2/Lab0_ED2/Models/CRUD.cs
+++ Lab0_ED2/Lab0_ED2/Models/CRUD.cs
@@ -8,14 +8,22 @@ namespace Lab0_ED2.Models
     public class CRUD : ICRUD
     {
         public List<Movie> Movies = new List<Movie>();
+        public List<Movie> MoviesComodin = new List<Movie>();
         
         public void Add(Movie New_Movie) {
-            
-            Movies.Add(New_Movie);
+            if (Movies.Count <=9)
+            {
+                Movies.Add(New_Movie);
+            }
+            else
+            {
+                MoviesComodin.Add(New_Movie);
+            }
         
         }
 
         public List<Movie> ReturnList() {
+            Movies.Reverse();
             return Movies;
 
         }
